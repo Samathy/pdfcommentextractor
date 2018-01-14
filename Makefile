6ccefbfcc00b8d4bcec130a48c97a6996972b5c6
Samathy Barratt <samathy@sbarratt.co.uk>
2018-01-14 00:25:21 +0000

    Changed to use poppler glib, since poppler-cpp doesnt support annotations.
    Kinda works at the moment, pulls text from highlights.
    However, doesnt seem to get /all/ the text from the highlights.

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
+IncludeDir = /usr/include
+
+
+
 poppler: | lib/poppler/build
-	cd lib/poppler/build/ && cmake .. -DBUILD_GTK_TESTS=OFF -DBUILD_QT5_TESTS=OFF -DBUILD_CPP_TESTS=ON -DENABLE_SPLASH=OFF -DENABLE_UTILS=OFF -DENABLE_CPP=ON -DENABLE_GLIB=OFF -DENABLE_GTK_DOC=OFF -DENABLE_QT5=OFF -DENABLE_LIBCURL=OFF -DENABLE_CMS=none -DENABLE_LIBOPENJPEG=none -DCMAKE_BUILD_TYPE=release
+	cd lib/poppler/build/ && cmake .. -DBUILD_GTK_TESTS=ON -DBUILD_QT5_TESTS=OFF -DBUILD_CPP_TESTS=ON -DENABLE_SPLASH=OFF -DENABLE_UTILS=OFF -DENABLE_CPP=ON -DENABLE_GLIB=ON -DENABLE_GTK_DOC=ON -DENABLE_QT5=OFF -DENABLE_LIBCURL=OFF -DENABLE_CMS=none -DENABLE_LIBOPENJPEG=none -DCMAKE_BUILD_TYPE=release
 	cd lib/poppler/build/ && make
 
 install-poppler:
@@ -18,8 +22,8 @@ clean_libs:
 
 
 pdfcommentextractor.cpp:
-	g++ -c -I/usr/include/poppler/cpp src/pdfcommentextractor.cpp -o build/pdfcommentextractor.o
+	g++ -c -ggdb -I$(IncludeDir)/poppler/glib -I$(IncludeDir)/glib-2.0/ -I/usr/lib/glib-2.0/include -I$(IncludeDir)/cairo/ src/pdfcommentextractor.cpp -o build/pdfcommentextractor.o
 
 all: |./build pdfcommentextractor.cpp
-	g++ -lpoppler -lpoppler-cpp build/pdfcommentextractor.o -opdfcommentextractor
+	g++ -ggdb -lpoppler -lpoppler-glib -lglib-2.0 build/pdfcommentextractor.o -opdfcommentextractor
 
--- src/pdfcommentextractor.cpp
+++ src/pdfcommentextractor.cpp
@@ -1,13 +1,71 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h> //realpath
+#include <string.h>
 
-#include "poppler-document.h"
-#include "poppler-page.h"
+#include <glib.h>
+#include <poppler.h>
+
 
 
 int main(int argv, char ** argc)
 {
 
-    poppler::document::load_from_file("");
+    GError * error = nullptr;
+
+    //TODO Add checks to make sure we have the right number of args
+    char * filename = realpath(argc[1], NULL);
+
+    //glib requires URIs (file:///home/foo/bar/baz.pdf)
+    gchar * filename_uri = g_filename_to_uri(filename,NULL , &error);
+    if (error != NULL)
+    {
+        std::cout << error->message << std::endl;
+        return 0;
+    }
+
+    free(filename);
+    g_clear_error(&error);
+
+    PopplerDocument * doc = poppler_document_new_from_file(filename_uri, NULL, &error);
+
+    if (error != NULL)
+    {
+        std::cout << error->message << std::endl;
+        return 0;
+    }
+
+
+    const int numPages = poppler_document_get_n_pages(doc);
+
+    std::cout << "Number of pages in the document: " << numPages << std::endl;
+
+    for (int i=0;i<numPages;i++)
+    {
+        PopplerPage * page = poppler_document_get_page(doc, i);
+
+        GList * annotations = poppler_page_get_annot_mapping(page);
+
+        for ( GList * l = annotations; l != NULL; l = l->next)
+        {
+            PopplerAnnotMapping * annotation = (PopplerAnnotMapping*)l->data;
+            PopplerAnnotType type = poppler_annot_get_annot_type(annotation->annot);
+            if (type == POPPLER_ANNOT_HIGHLIGHT)
+            {
+                PopplerRectangle rect;
+                poppler_annot_get_rectangle(annotation->annot, &rect);
+                char * text = poppler_page_get_text_for_area(page, &rect);
+                std::cout << text;
+                return 0 ;
+            }
+
+        }
+
+        poppler_free_page_annot_mapping(page);
+
+    }
+
+
+    return 0;
 
 }
